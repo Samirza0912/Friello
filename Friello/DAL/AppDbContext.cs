4c5e15f63976e08287dd4aad19dcece112c7f77e
Samirza0912 <samirza@code.edu.az>
2022-06-21 17:26:30 +0400

    feat:format

---
+++ Friello/DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Friello.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+
+       // public DbSet<> MyProperty { get; set; }
+    }
+}
